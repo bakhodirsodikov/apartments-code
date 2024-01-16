ea5a145a1ce20369f77cfe1ef1c373ac250560c4
bakhodir <bakhodir.sodikov2002@mail.ru>
2024-01-16 12:38:24 +0500

    first for code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,121 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid text-center" >
+    <header class="header">
+      <ul class="nav nav-pills justify-content-center">
+        <li class="nav-item" v-for="(titles, id) in header" :key="id">
+          <a class="nav-link " aria-current="page" href="#" 
+          @click="headerClicked(id)" 
+          :class="{active : clickedLinkInHeader == titles }">{{ titles }}</a>
+        </li>
+      </ul>
+    </header>
+    <main v-if="clickedLinkInHeader === 'Шахматка'">
+      <div class="filter__container d-flex flex-column flex-md-row my-4 justify-content-md-between">
+        <div class="room__filter-container d-flex flex-column justify-content-start m-2">
+          <p class="m-0">Кол-во комнаты</p>
+          <div class="number__rooms d-flex align-items-center">
+            <div class="num__container d-flex align-items-center">
+              <label for="appartOne" class="checkbox">
+                <input type="checkbox" class="checkbox__input" id="appartOne" v-model="filters.appartOne">
+                <span class="checkbox__box">1 </span>
+              </label>
+              <label for="appartTwo" class="checkbox">
+                <input type="checkbox" class="checkbox__input" id="appartTwo" v-model="filters.appartTwo">
+                <span class="checkbox__box">2</span>
+              </label>
+              <label for="appartThree" class="checkbox">
+                <input type="checkbox" class="checkbox__input" id="appartThree" v-model="filters.appartThree">
+                <span class="checkbox__box">3</span>
+              </label>
+              <label for="appartFour" class="checkbox">
+                <input type="checkbox" class="checkbox__input" id="appartFour" v-model="filters.appartFour">
+                <span class="checkbox__box">4</span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div class="room__filter-container d-flex flex-column justify-content-start m-2">
+          <p class="m-0">Тип квартиры</p>
+          <div class="dropdown filter__dropdown">
+            <a class="btn btn-info dropdown-toggle w-100" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              {{ typeApparts }}
+            </a>
+            <ul class="dropdown-menu w-100 dropdown__content">
+              <li><a class="dropdown-item" href="#" 
+                v-for="(type, idx) in type" 
+                :key="idx"
+                @click="typeApparts = type">
+                  {{ type }}
+                </a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="room__filter-container d-flex flex-column justify-content-start m-2">
+          <p class="m-0">Площадь в кв.м</p>
+          <div class="input-group d-flex">
+            <input type="number" class="form-control square__filter" placeholder="от" v-model="filters.squareFrom">
+            <input type="number" class="form-control square__filter" placeholder="до" v-model="filters.squareBefore">
+          </div>
+        </div>
+        <div class="color-meanig d-flex align-items-center">
+          <div class="d-flex flex-row mx-1">
+            <span class="color free"></span> &nbsp;
+            <p class="m-0">Свободно</p>
+          </div>
+          <div class="d-flex flex-row mx-1">
+            <span class="color booked"></span> &nbsp;
+            <p class="m-0">Забронированные</p>
+          </div>
+          <div class="d-flex flex-row mx-1">
+            <span class="color sold"></span> &nbsp;
+            <p class="m-0">Проданные</p>
+          </div>
+          <div class="d-flex flex-row mx-1">
+            <span class="color not-available"></span> &nbsp;
+            <p class="m-0">Недоступные</p>
+          </div>
+        </div>
+      </div>
+      <div class="content__container d-flex flex-column">
+        <div class="info__container d-flex justify-content-around">
+          <h6>Дом: {{nameAppart}}</h6>
+          <h6>Найдено: {{ foundApparts }}</h6>
+          <h6>Свободно: {{ freeApparts }}</h6>
+        </div>
+        <div class="content d-flex ">
+          <div class="num__of-loor d-flex flex-column">
+            <p class="">№</p>
+            <div class="num-floor d-flex flex-column-reverse">
+              <p class="" v-for="num in floorForNum" :key="num.id">{{ num }}</p>
+            </div>
+          </div>
+          <div class="entrance d-flex flex-column border-end" 
+          v-for="(entrance, idx) in house" 
+          :key="idx">
+            <div class="mb-3">Подъезд {{ idx + 1 }}</div>
+            <div class="floors-container d-flex flex-column-reverse h-100">
+              <div class="floor d-flex justify-content-around mb-3" v-for="floor in entrance" :key="floor.id">
+                <div class="apparts d-flex justify-content-center" 
+                v-for="appart in floor" :key="appart.room">
+                  <span class="not-available"
+                  :class="{'free' : appart.free === 'yes', 'booked' : appart.free === 'booked', 'sold' : appart.free === 'sold', 'disabled': appart.disabled == true}">
+                    {{ appart.room }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="./style/app.css">
+
 </style>
+
+<script src="./main.js">
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,549 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
+export default{
+  name: 'App',
+  data(){
+    return {
+      house: [
+          [ //entrance
+            [ //floor
+              { //apartment
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'sold',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 1,
+                square: 50,
+                free: 'sold',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [ //floor
+              { //apartment
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'sold',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 1,
+                square: 95,
+                free: 'booked',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 1,
+                square: 50,
+                free: 'sold',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'sold',
+                disabled: true
+              }
+            ],
+            [ //floor
+              { //apartment
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'booked',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'booked',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+          ],
+          [ //entrance
+            [ //floor
+              { //apartment
+                room: 1,
+                square: 50,
+                free: 'booked',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'booked',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'booked',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 2,
+                square: 50,
+                free: 'booked',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 65,
+                free: 'sold',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [ //floor
+              { //apartment
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'booked',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'not-available',
+                disabled: true
+              },
+              {
+                room: 1,
+                square: 95,
+                free: 'booked',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 1,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [ //floor
+              { //apartment
+                room: 1,
+                square: 50,
+                free: 'sold',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 95,
+                free: 'sold',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 3,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 85,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 4,
+                square: 95,
+                free: 'yes',
+                disabled: true
+              }
+            ],
+            [
+              //apartment
+              {
+                room: 2,
+                square: 50,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 65,
+                free: 'yes',
+                disabled: true
+              },
+              {
+                room: 2,
+                square: 85,
+                free: 'booked',
+                disabled: true
+              },
+              {
+                room: 3,
+                square: 95,
+                free: 'sold',
+                disabled: true
+              }
+            ],
+          ]
+      ],
+      header: ['Шахматка', 'Плитка', 'Список', 'Планировки', 'Планировки этажей', 'Фасады'],
+      clickedLinkInHeader: '',
+      numberOfRooms: [1, 2, 3, 4],
+      numberOfRoomsForFilter: 'Выберите кол-во комнаты',
+      type: ['Студия'],
+      typeApparts: 'Выбрать',
+      nameAppart: '',
+      foundApparts: null,
+      freeApparts: null,
+      floorForNum: [1,2,3,4,5,6,7,8,9],
+      filters: {
+        appartOne: false,
+        appartTwo: false,
+        appartThree: false,
+        appartFour: false,
+        squareFrom: null,
+        squareBefore: null
+      },
+    }
+  },
+
+  watch: {
+    filters: {
+      deep: true,
+      handler: function() {
+        this.filterHouse();
+      }
+    }
+  },
+
+  methods: {
+    headerClicked(id){
+      this.clickedLinkInHeader = this.header[id]
+    },
+    
+    filterHouse(){
+      this.house.forEach((entrance, idx) => {
+        entrance.forEach((floor, id) => {
+          floor.forEach((appart, idx) => {
+            if ((this.filters.appartOne && appart.room === 1) || 
+              (this.filters.appartTwo && appart.room === 2) || 
+              (this.filters.appartThree && appart.room === 3) || 
+              (this.filters.appartFour && appart.room === 4)) {
+              appart.disabled = false
+            }
+            else{
+              appart.disabled = true
+            }
+          })
+        })
+      });
+    }
+  }
+}
 createApp(App).mount('#app')
